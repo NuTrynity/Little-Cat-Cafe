840d3d7f9e5b138b4cafe37a9604aeeb0b960c0d
NuTrynity <vargasromeo716@gmail.com>
2023-11-11 21:26:21 +0800

    added a color randomizer

--- Scripts/CharacterScripts/Cat.gd
+++ Scripts/CharacterScripts/Cat.gd
@@ -1,3 +1,15 @@
 extends CharacterBody2D
 
-@export var cat_sheet : Resource
+@export var cat_sheet : Array
+
+var randomizer = RandomNumberGenerator.new()
+
+#this randomizes the color of the cats :D
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		change_color()
+
+func change_color():
+	var rando_color = randomizer.randi_range(0, 2)
+	$CatSprite.modulate = cat_sheet[rando_color]
+	print(rando_color)
--- Scripts/ResourceScripts/CatSheetScript.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-class_name CatSheet
-
-@export var cat_textures : Array
-@export var move_speed : float
