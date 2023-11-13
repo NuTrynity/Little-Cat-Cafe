b5241832ffa596eba8d6a762026975e623cb94f2
NuTrynity <vargasromeo716@gmail.com>
2023-11-13 20:24:27 +0800

    added a patience bar to npcs and added the background with collisions

--- Scripts/CharacterScripts/NPC.gd
+++ Scripts/CharacterScripts/NPC.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 @export var player_resources : PlayerMealCarry
 
 @onready var interact_area = $InteractionArea
+@onready var patience_bar = $Patience
+
+var patience : float
 
 func _ready():
 	interact_area.interact = Callable(self, "_on_player_give_meal")
+	patience_bar.global_position.y -= 128
 
 func _on_player_give_meal():
 	if player_resources.carry_amt > 0:
