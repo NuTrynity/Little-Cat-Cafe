42626a836671d475351d596e8e41dfb57a226681
NuTrynity <vargasromeo716@gmail.com>
2023-11-14 13:25:53 +0800

    you can now cook

--- Scripts/ObjectScripts/fryingpan.gd
+++ Scripts/ObjectScripts/fryingpan.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-@onready var interact_area
+@onready var interact_area = $InteractionArea
 
 var meal = preload("res://Nodes/Templates/meal_template.tscn")
 
 func _ready():
-	interact_area.interact = Callable(self, "_on_interaction") #TODO LOOKS FOR WHY THIS IS BROKEN
+	interact_area.interact = Callable(self, "_on_interaction")
 
 func _on_interaction():
 	var food = meal.instantiate()
-	food.positionx += 36
+	food.position.x += 360
 	add_child(food)
