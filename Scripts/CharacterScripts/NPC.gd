583818e22aa4be49acc2606248c3675495eff378
NuTrynity <vargasromeo716@gmail.com>
2023-11-14 15:39:53 +0800

    added animations

--- Scripts/CharacterScripts/NPC.gd
+++ Scripts/CharacterScripts/NPC.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 @export var player_resources : PlayerMealCarry
 @export var move_speed : float = 300.0
 
+@onready var animations = $NPCAnimations
 @onready var npc_sprite = $NPCSprite
 @onready var interact_area = $InteractionArea
 @onready var patience_bar = $Patience
+@onready var meal_want = $MealWanted
 @onready var patience_timer = Timer.new()
+@onready var eat_timer = Timer.new()
 
 var patience : float
 var satisfied : bool = false
@@ -31,9 +34,13 @@ func _physics_process(_delta : float) -> void:
 	
 	#NPC State Block
 	if is_sitting == true:
+		animations.play("sitting")
+		meal_want.show()
 		patience_bar.show()
 		npc_sprite.texture = load("res://Sprites/customer sprites/customer1_sitting.png")
 	else:
+		animations.play("walking")
+		meal_want.hide()
 		patience_bar.hide()
 		npc_sprite.texture = load("res://Sprites/customer sprites/customer1_standing.png")
 
@@ -42,6 +49,11 @@ func patience_timer_setup():
 	patience_timer.one_shot = false
 	patience_timer.wait_time = 0.1
 	patience_timer.connect("timeout", _on_timer_timeout)
+	
+	add_child(eat_timer)
+	eat_timer.one_shot = true
+	eat_timer.wait_time = 3
+	eat_timer.connect("timeout", on_meal_finished)
 
 func _on_timer_timeout():
 	patience -= 1
@@ -61,6 +73,9 @@ func _on_player_give_meal():
 		print("Thank you for the food sister, I'm going now")
 	
 	print(player_resources.carry_amt)
+
+func on_meal_finished():
+	pass
 
 func _on_chair_detector_area_entered(area):
 	if area.is_in_group("chair"):
--- Scripts/CharacterScripts/Player.gd
+++ Scripts/CharacterScripts/Player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @export var player_stats : Resource
 @export var move_speed : float = 500.0
 
+@onready var pivot : Node2D = $PlayerSkin
+@onready var start_scale : Vector2 = pivot.scale
+@onready var animations = $PlayerSkin/PlayerAnimations
+
 var input = Vector2.ZERO
 
 func _physics_process(_delta):
@@ -10,6 +14,17 @@ func _physics_process(_delta):
 	
 	velocity = (input * move_speed)
 	move_and_slide()
+	
+	var is_idle = is_zero_approx(velocity.x) and is_zero_approx(velocity.y)
+	var is_walking = not Vector2.ZERO
+	
+	if is_idle:
+		animations.play("idle")
+	elif  is_walking:
+		animations.play("walking")
+	
+	if not is_zero_approx(velocity.x):
+		pivot.scale.x = sign(velocity.x) * start_scale.x
 
 func get_input():
 	input.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
