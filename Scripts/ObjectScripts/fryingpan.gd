cfb996bb1a26d2a15f3589d267bc2f0ecf8fbe10
eyg6062 <anele9999@gmail.com>
2023-11-15 02:17:34 -0500

    added multiple spots to pickup area

--- Scripts/ObjectScripts/fryingpan.gd
+++ Scripts/ObjectScripts/fryingpan.gd
@@ -8,6 +8,15 @@ func _ready():
 	interact_area.interact = Callable(self, "_on_interaction")
 
 func _on_interaction():
-	var food = meal.instantiate()
-	food.position.x += 360
-	add_child(food)
+	var placement_pts = get_tree().get_root().get_node("scene_0/KitchenCounter2/PickupTablecloth/PlacementPoints")
+	var pickup_full = true
+	for point in placement_pts.get_children():
+		if point.get_is_empty():
+			var food = meal.instantiate()
+			add_child(food)
+			point.add_item(food)
+			pickup_full = false
+			break
+	if pickup_full:
+		print("can't cook, pickup is full")
+
---
+++ Scripts/ObjectScripts/placement_point.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+var is_empty : bool = true
+var held_item : Node2D = null
+	
+func get_is_empty() -> bool:
+	return is_empty
+	
+func add_item(item:Node2D) -> void:
+	held_item = item
+	held_item.global_position = global_position
+	is_empty = false
+	
+func remove_item() -> Node2D:
+	held_item = null
+	is_empty = true
+	return held_item
