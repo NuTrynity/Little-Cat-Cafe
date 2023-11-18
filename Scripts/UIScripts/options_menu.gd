b99022f8f76e09642b17b03d6cfb958691c5a7d4
NuTrynity <vargasromeo716@gmail.com>
2023-11-18 23:21:32 +0800

    added some menus

--- Scripts/UIScripts/options_menu.gd
+++ Scripts/UIScripts/options_menu.gd
@@ -1,1 +1,7 @@
-extends Control
+extends PanelContainer
+
+signal back
+
+func _on_back_pressed():
+	hide()
+	back.emit()
--- Scripts/UIScripts/pause_menu.gd
+++ Scripts/UIScripts/pause_menu.gd
@@ -1,6 +1,13 @@
 extends Control
 
+@onready var options_menu = $CenterContainer/Menu/OptionsMenu
 var paused : bool = true #this works I dunno why
+
+func _ready():
+	options_menu.back.connect(_show_buttons)
+
+func _show_buttons():
+	$CenterContainer/Menu/Buttons.show()
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -18,7 +25,20 @@ func pause_game():
 	paused = !paused
 
 func _on_quit_pressed():
-	get_tree().quit()
+	$CenterContainer/Confirmation.show()
+	$CenterContainer/Menu.hide()
 
 func _on_resume_pressed():
 	pause_game()
+
+func _on_options_pressed():
+	$CenterContainer/Menu/Buttons.hide()
+	$CenterContainer/Menu/OptionsMenu.show()
+
+func _on_yes_pressed():
+	get_tree().change_scene_to_file("res://Nodes/UI/main_menu.tscn")
+	get_tree().paused = false
+
+func _on_no_pressed():
+	$CenterContainer/Confirmation.hide()
+	$CenterContainer/Menu.show()
