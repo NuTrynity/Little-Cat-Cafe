41ec8bff49ca8166d40e54db60c7af52e2b17836
NuTrynity <vargasromeo716@gmail.com>
2023-11-18 23:24:53 +0800

    forgot the main menu options

--- Scripts/UIScripts/main_menu.gd
+++ Scripts/UIScripts/main_menu.gd
@@ -2,16 +2,25 @@ extends Control
 
 @onready var main_menu_bg = preload("res://Assets/Music/Main Menu.wav")
 @onready var main_menu_stream = $MainMenuMusic
+@onready var options_menu = $CenterContainer/OptionsMenu
 
 func _ready():
 	$CenterContainer/Buttons/Play.grab_focus()
+	options_menu.back.connect(_show_menu)
 	main_menu_stream.play()
+
+func _show_menu():
+	$CenterContainer/Buttons.show()
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scene_0.tscn")
 
 func _on_how_to_play_pressed():
 	pass # Replace with function body.
+
+func _on_options_pressed():
+	$CenterContainer/Buttons.hide()
+	$CenterContainer/OptionsMenu.show()
 
 func _on_quit_pressed():
 	$CenterContainer/Confirmation/Container/Buttons/No.grab_focus()
