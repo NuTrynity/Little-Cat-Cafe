9fcd22840b1fa5e5fe552b28dbf15359e05d2865
eyg6062 <anele9999@gmail.com>
2023-11-18 05:59:27 -0500

    made cat idle movement

--- Scripts/CharacterScripts/Cat.gd
+++ Scripts/CharacterScripts/Cat.gd
@@ -1,28 +1,63 @@
 extends CharacterBody2D
 
-@onready var nav_mesh : NavigationRegion2D = get_tree().get_root().get_node("scene_0/NavigationRegion2D")
+
 
 enum State {IDLE, ACT, HUNGRY, REST }
+var state = State.IDLE as State
+
+enum IdleState {STAND, WALK }
+var idleState = IdleState.STAND as State
+
+@onready var standing_timer := Timer.new() as Timer
+
+@onready var aiMvt := $AiMovement as CatAiMovement
+
+var rng := RandomNumberGenerator.new() as RandomNumberGenerator
 
 func _ready():
-	pass
+	add_child(standing_timer)
+	standing_timer.one_shot = true
+	standing_timer.connect("timeout", _on_standing_end)
+	start_standing_timer()
+	
 	
 func _physics_process(_delta : float) -> void:
-	pass
+	match state:
+		State.IDLE:
+			idle()
+			pass
+		State.ACT:
+			pass
+		State.HUNGRY:
+			pass
+		State.REST:
+			pass
 
-func choose_random_point() -> Vector2:
-	var rng = RandomNumberGenerator.new()
-	var random_idx = rng.randi_range(0, nav_mesh.navigation_polygon.get_polygon_count()-1)
+# called in process
+func idle():
+	# timer of random length for how long to idle
+	if (idleState == IdleState.STAND):
+		return
+	# set a random target and walk towards it
+	if (idleState == IdleState.WALK):
+		var meep = aiMvt.target
+		aiMvt.approach_target()
+		if (aiMvt.reached_target()):
+			idleState = IdleState.STAND
+			start_standing_timer()
+		
+func _on_standing_end():
+	idleState = IdleState.WALK
+	aiMvt.target_random_point()
 	
-	var polygon_idx = nav_mesh.navigation_polygon.get_polygon(random_idx) as PackedInt32Array
-	var all_vertices = nav_mesh.navigation_polygon.get_vertices()
+
+func start_standing_timer():
+	standing_timer.wait_time = rng.randf_range(5.0, 10.0)
+	standing_timer.start()
 	
-	var polygon_array : PackedVector2Array = []
-	for vertex in polygon_idx:
-		polygon_array.append(all_vertices[vertex])
+	pass
 	
-	var randomPoint = PolygonRandomPointGenerator.new(polygon_array)
-	
-	return randomPoint.get_random_point()
+
 	
 	
+
--- Scripts/CharacterScripts/cat_ai_movement.gd
+++ Scripts/CharacterScripts/cat_ai_movement.gd
@@ -1,9 +1,38 @@
 extends AiMovement
+class_name CatAiMovement
 
-@export var cat_speed : int = 150
+@export var cat_speed : int = 80
+
+@onready var nav_mesh : NavigationRegion2D = get_tree().get_root().get_node("scene_0/NavigationRegion2D")
+
+var target_point : Node2D
 
 func _ready():
 	speed = cat_speed
 	
+	target_point = Node2D.new()
+	add_child(target_point)
+	
+	target = target_point
+	
 func _on_timer_timeout():
 	pass
+	
+func target_random_point():
+	target_point.global_position = choose_random_point()
+	makepath()
+
+func choose_random_point() -> Vector2:
+	var rng = RandomNumberGenerator.new()
+	var random_idx = rng.randi_range(0, nav_mesh.navigation_polygon.get_polygon_count()-1)
+	
+	var polygon_idx = nav_mesh.navigation_polygon.get_polygon(random_idx) as PackedInt32Array
+	var all_vertices = nav_mesh.navigation_polygon.get_vertices()
+	
+	var polygon_array : PackedVector2Array = []
+	for vertex in polygon_idx:
+		polygon_array.append(all_vertices[vertex])
+	
+	var randomPoint = PolygonRandomPointGenerator.new(polygon_array)
+	
+	return randomPoint.get_random_point()
