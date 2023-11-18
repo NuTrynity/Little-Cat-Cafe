c47b84e30e186c3dfe43f15718411e8f3684ddfc
eyg6062 <anele9999@gmail.com>
2023-11-18 18:05:16 -0500

    added cooldown to cat

--- Scripts/CharacterScripts/Cat.gd
+++ Scripts/CharacterScripts/Cat.gd
@@ -3,19 +3,25 @@ extends CharacterBody2D
 enum State {IDLE, APPROACH, ACT, HUNGRY, REST }
 var state = State.IDLE as State
 
-enum IdleState {STAND, WALK }
+enum IdleState {STAND, WALK}
 var idleState = IdleState.STAND as State
 
 @export var pause_duration : float = 4.0
 
+@export var idle_speed : int = 70
+@export var approach_speed : int = 110
+@export var cd_duration : float = 4.0
+
 @onready var standing_timer := Timer.new() as Timer
 @onready var pause_timer := Timer.new() as Timer
+@onready var cd_timer := Timer.new() as Timer
 
 @onready var aiMvt := $AiMovement as CatAiMovement
 
 var rng := RandomNumberGenerator.new() as RandomNumberGenerator
 
 var patience_timer : Timer = null
+var on_cd : bool = false
 
 func _ready():
 	add_child(standing_timer)
@@ -27,14 +33,17 @@ func _ready():
 	pause_timer.one_shot = true
 	pause_timer.connect("timeout", _on_pause_end)
 	
+	add_child(cd_timer)
+	cd_timer.one_shot = true
+	cd_timer.connect("timeout", _on_cd_end)
 	
 func _physics_process(_delta : float) -> void:
-	print(state)
 	match state:
 		State.IDLE:
 			idle()
-			if (aiMvt.try_target_customer()):
-				state = State.APPROACH
+			if (!on_cd):
+				if (aiMvt.try_target_customer()):
+					to_approach()
 			
 		State.APPROACH:
 			if !aiMvt.can_target_customer():
@@ -50,6 +59,7 @@ func _physics_process(_delta : float) -> void:
 				aiMvt.untarget_customer()
 				to_idle_walk()
 				pause_timer.stop()
+				start_cd_timer()
 			
 		State.HUNGRY:
 			pass
@@ -68,14 +78,20 @@ func idle():
 			to_idle_stand()
 		
 func to_idle_stand():
+	aiMvt.speed = idle_speed
 	state = State.IDLE
 	idleState = IdleState.STAND
 	start_standing_timer()
 	
 func to_idle_walk():
+	aiMvt.speed = idle_speed
 	state = State.IDLE
 	idleState = IdleState.WALK
 	aiMvt.target_random_point()
+
+func to_approach():
+	state = State.APPROACH
+	aiMvt.speed = approach_speed
 
 func to_act():
 	state = State.ACT
@@ -91,6 +107,11 @@ func start_standing_timer():
 func start_pause_timer():
 	pause_timer.wait_time = pause_duration
 	pause_timer.start()
+	
+func start_cd_timer():
+	on_cd = true
+	cd_timer.wait_time = cd_duration
+	cd_timer.start()
 
 func _on_standing_end():
 	to_idle_walk()
@@ -100,6 +121,10 @@ func _on_pause_end():
 	aiMvt.target.patience_timer.set_paused(false)
 	aiMvt.untarget_customer()
 	to_idle_walk()
+	start_cd_timer()
+	
+func _on_cd_end():
+	on_cd = false
 	
 	
 
