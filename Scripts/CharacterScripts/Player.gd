e5fb6f6615ef275ce5a1d3a76bb102c9ad58498c
NuTrynity <vargasromeo716@gmail.com>
2023-11-25 04:19:05 +0800

    commented out the mobile tap movement

--- Scripts/CharacterScripts/Player.gd
+++ Scripts/CharacterScripts/Player.gd
@@ -11,7 +11,7 @@ extends CharacterBody2D
 var input = Vector2.ZERO
 var click_position = Vector2()
 var tap_movement : bool
-
+'''
 func _ready():
 	click_position = global_position
 
@@ -19,22 +19,22 @@ func _input(event):
 	if event.is_action_pressed("click_move"):
 		click_position = get_global_mouse_position()
 		make_path()
-
+'''
 func _physics_process(_delta):
-	'''
+	
 	get_input()
 	
 	velocity = input * move_speed
 	move_and_slide()
-	'''
 	
+	'''
 	if !reached_position():
 		var direction = to_local(nav_agent.get_next_path_position()).normalized()
 		velocity = direction * move_speed
 		move_and_slide()
 	else:
 		velocity = Vector2.ZERO
-	
+	'''
 	var is_idle = is_zero_approx(velocity.x) and is_zero_approx(velocity.y)
 	var is_walking = not Vector2.ZERO
 	
@@ -45,7 +45,7 @@ func _physics_process(_delta):
 	
 	if not is_zero_approx(velocity.x):
 		pivot.scale.x = sign(velocity.x) * start_scale.x
-'''
+
 func get_input():
 	input.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	input.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
@@ -57,3 +57,4 @@ func make_path() -> void:
 
 func reached_position():
 	return nav_agent.distance_to_target() <= 4
+'''
