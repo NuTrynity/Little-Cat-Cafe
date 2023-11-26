1af93a06352ce9f3c0e80597b1682b850f6f5ef3
NuTrynity <vargasromeo716@gmail.com>
2023-11-26 21:29:51 +0800

    removed tap to move area movement

--- Scripts/CharacterScripts/Player.gd
+++ Scripts/CharacterScripts/Player.gd
@@ -11,29 +11,13 @@ extends CharacterBody2D
 var input = Vector2.ZERO
 var click_position = Vector2()
 var tap_movement : bool
-'''
-func _ready():
-	click_position = global_position
 
-func _input(event):
-	if event.is_action_pressed("click_move"):
-		click_position = get_global_mouse_position()
-		make_path()
-'''
 func _physics_process(_delta):
 	input = get_input()
 	
 	velocity = input * move_speed
 	move_and_slide()
 	
-	'''
-	if !reached_position():
-		var direction = to_local(nav_agent.get_next_path_position()).normalized()
-		velocity = direction * move_speed
-		move_and_slide()
-	else:
-		velocity = Vector2.ZERO
-	'''
 	var is_idle = is_zero_approx(velocity.x) and is_zero_approx(velocity.y)
 	var is_walking = not Vector2.ZERO
 	
@@ -50,10 +34,3 @@ func get_input():
 	input.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	
 	return input.normalized()
-'''
-func make_path() -> void:
-	nav_agent.target_position = click_position
-
-func reached_position():
-	return nav_agent.distance_to_target() <= 4
-'''
