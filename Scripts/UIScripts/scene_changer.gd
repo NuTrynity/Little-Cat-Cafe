dbf31c9a559b000df05342514b423cffcad561a5
NuTrynity <vargasromeo716@gmail.com>
2023-12-22 16:59:29 +0800

    added warning signs

--- Scripts/UIScripts/scene_changer.gd
+++ Scripts/UIScripts/scene_changer.gd
@@ -1,8 +1,10 @@
 extends CanvasLayer
-
 @onready var animations = $Animations
 @onready var sprite = $Sprite
-
+'''
+Case Sensitive! make sure anim parameter has correct spelling and
+exists on animation player node when calling this function
+'''
 func change_scene(target : String, anim : String):
 	var img = get_viewport().get_texture().get_image()
 	var screenshot = ImageTexture.create_from_image(img)
