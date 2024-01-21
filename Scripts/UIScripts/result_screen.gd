c211bf72b112403ab4c68b2c35b7daa6eb6628cb
NuTrynity <vargasromeo716@gmail.com>
2024-01-21 14:47:37 +0800

    added counting system on the result screen

--- Scripts/UIScripts/result_screen.gd
+++ Scripts/UIScripts/result_screen.gd
@@ -14,6 +14,31 @@ func _ready():
 	
 	money.text = str(GlobalScript.cash_on_hand) + " $"
 	progress_bar.value = player_resource.rating
+	
+	##Items Owned Sections##
+	%cats.text = "Cats Owned: " + str(count_all(GlobalScript.shop))
+	%tables.text = "Tables Owned: " + str(count_specific(GlobalScript.items_owned["table"]))
+	%chefcat.text = "Chef Cats Owned: " + str(count_specific(GlobalScript.shop[0]))
+	%greycat.text = "Grey Cats Owned: " + str(count_specific(GlobalScript.shop[1]))
+	%tabbycat.text = "Tabby Cats Owned: " + str(count_specific(GlobalScript.shop[2]))
+	%browncat.text = "Brown Cats Owned: " + str(count_specific(GlobalScript.shop[3]))
+	%orangecat.text = "Orange Cats Owned: " + str(count_specific(GlobalScript.shop[4]))
+	##Items Owned Sections##
+
+func count_all(data): #Counts all owned items in a Dictionary
+	var amt : int
+	var items = data
+	for item_index in items:
+		for x in range(items[item_index]["owned"]):
+			amt += 1
+	return amt
+
+func count_specific(data): #Counts only a specific owned item in a Dictionary
+	var amt : int
+	var items = data
+	for x in range(data["owned"]):
+		amt += 1
+	return amt
 
 func _on_next_day_pressed():
 	GlobalScript.days += 1
