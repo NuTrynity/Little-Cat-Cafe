7d5416f6ac37f595aeda031696d7ca88b1c88fb6
NuTrynity <88869373+NuTrynity@users.noreply.github.com>
2024-04-27 11:02:13 +0800

    changed some UI animations to tweens

--- Scripts/UIScripts/meal_price_on_buy.gd
+++ Scripts/UIScripts/meal_price_on_buy.gd
@@ -3,6 +3,10 @@ extends Label
 @onready var delete_timer = Timer.new()
 
 func _ready():
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT_IN)
+	
+	tween.tween_property(self, "global_position:y", global_position.y - 256, 1.2)
+	
 	text = "+ " + price + "$"
 	
 	add_child(delete_timer)
@@ -10,9 +14,6 @@ func _ready():
 	delete_timer.one_shot = true
 	delete_timer.connect("timeout", on_delete_timeout)
 	delete_timer.start()
-
-func _process(_delta):
-	position.y -= 4
 
 func on_delete_timeout():
 	queue_free()
